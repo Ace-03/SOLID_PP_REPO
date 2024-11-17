cf250a70ed8f3142bd061b1223fe4c24a8119762
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2024-11-17 16:26:02 -0700

    added project

---
+++ SOLID PP Project/Assets/_Project/Scripts/SelectionManager.cs
@@ -0,0 +1,40 @@
+﻿﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    private IRayProvider _rayProvider;
+    private ISelector _selector;
+    private ISelectionResponse _selectionRespones;
+
+    private Transform _currentSelection;
+    
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+
+        _rayProvider = GetComponent<IRayProvider>();
+        _selector = GetComponent<ISelector>();
+        _selectionRespones = GetComponent<ISelectionResponse>();
+    }
+
+    private void Update()
+    {
+        if (_currentSelection != null) _selectionRespones.OnDeselect(_currentSelection);
+
+        _selector.Check(_rayProvider.CreateRay());
+        _currentSelection = _selector.GetSelection();
+
+        if (_currentSelection != null)  _selectionRespones.OnSelect(_currentSelection);
+        
+    }
+}
+
+
+
+
+
+
+
